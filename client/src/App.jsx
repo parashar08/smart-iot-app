5d9f219227cc00818b9a3f18ca7ba111639b9234
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-05-14 19:19:51 +0530

    learn auth using firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import { getDatabase, ref, set } from 'firebase/database';
 import { app } from './firebase';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import Auth from './Auth';
 
 const db = getDatabase(app);
+const auth = getAuth(app);
 
 function App() {
   const putData = () => {
@@ -11,10 +14,34 @@ function App() {
       age: 21
     })
   }
+
+  const RegisterUser = () => {
+    createUserWithEmailAndPassword(auth, 'the69bit@gmail.com', 'pass@123')
+      .then(res => console.log(res))
+      .catch(error => console.log(`Error code ${error.code} and Error message ${error.message}`))
+  }
   return (
     <div className='container'>
       <p>In the app component!</p>
       <button onClick={putData}>send data</button>
+
+      <div>
+            <p>In auth section of our application!</p>
+            <form >
+                <div className='input-container'>
+                    <label htmlFor="email"></label>
+                    <input type="email" />
+                </div>
+                <div className='input-container'>
+                    <label htmlFor="password"></label>
+                    <input type="password" />
+                </div>
+            </form>
+
+            <button onClick={RegisterUser}>Signup</button>
+        </div>
+
+        <Auth />
     </div>
   )
 }
